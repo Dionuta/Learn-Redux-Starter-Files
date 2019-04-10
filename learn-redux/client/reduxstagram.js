11d5c7085ae2a00efcfb2e4736b600034cdc9636
Dionuta <diounta@gmail.com>
2019-04-10 01:45:36 -0700

    Component Setup

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
+
 
 //Import CSS
+import css from './styles/style.styl';
 
+//Import Components
+import Main from './components/Main'
+
+ReactDOM.render(<Main />, document.getElementById('root'));
